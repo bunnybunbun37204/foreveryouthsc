cefcf4194d610bc7758d099fbd39d24eef882118
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-05-22 03:08:52 +0700

    init project

---
+++ src/firebase/client.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3yqzZjHWChlBiK0m_0s5GfuDRnLe6-v8",
+  authDomain: "foreveryouthsc.firebaseapp.com",
+  projectId: "foreveryouthsc",
+  storageBucket: "foreveryouthsc.appspot.com",
+  messagingSenderId: "418839446227",
+  appId: "1:418839446227:web:99004d90999f0da9058a26",
+  measurementId: "G-DEFFW7K09S",
+};
+
+export const app = initializeApp(firebaseConfig);
