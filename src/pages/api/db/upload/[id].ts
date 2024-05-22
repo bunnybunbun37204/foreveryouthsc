a6349f3d347a98ff589f4364af07534ce3e5c937
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-05-23 05:10:23 +0700

    feat: add upload image

---
+++ src/pages/api/db/upload/[id].ts
@@ -0,0 +1,88 @@
+import type { APIRoute } from "astro";
+import { app } from "../../../../firebase/server";
+import { getFirestore } from "firebase-admin/firestore";
+import { Buffer } from 'buffer'; // Import buffer
+
+const db = getFirestore(app);
+const foreveryouthRef = db.collection("foreveryouth");
+
+interface UploadResponse {
+  key: string;
+}
+
+
+export const POST: APIRoute = async ({ params, redirect, request }) => {
+  const formData = await request.formData();
+  const description = formData.get("description")?.toString();
+  const imageFile = formData.get("image") as File | null;
+
+  if (!imageFile) {
+    return new Response("Missing image file", {
+      status: 400,
+    });
+  }
+
+  // Read file and convert to base64
+  const arrayBuffer = await imageFile.arrayBuffer();
+  const base64Image = Buffer.from(arrayBuffer).toString('base64');
+  const response = await fetch(
+    "https://smo-api.bunyawatapp37204.workers.dev/images/upload",
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        body: base64Image,
+        width: 300,
+        height: 300,
+      }),
+    },
+  );
+  const jsonData: unknown = await response.json();
+  const _data = jsonData as UploadResponse;
+  const img =
+    "https://smo-api.bunyawatapp37204.workers.dev/images/" + _data.key;
+  if (!description) {
+    return new Response("Missing required fields", {
+      status: 400,
+    });
+  }
+
+  if (!params.id) {
+    return new Response("Cannot find friend", {
+      status: 404,
+    });
+  }
+
+  try {
+    await foreveryouthRef.doc(params.id).create({
+      description: description,
+      image_url: img
+    });
+  } catch (error) {
+    return new Response("Something went wrong", {
+      status: 500,
+    });
+  }
+
+  return redirect("/dashboard");
+};
+
+export const DELETE: APIRoute = async ({ params, redirect }) => {
+  if (!params.id) {
+    return new Response("Cannot find friend", {
+      status: 404,
+    });
+  }
+
+  try {
+    await foreveryouthRef.doc(params.id).delete();
+  } catch (error) {
+    return new Response("Something went wrong", {
+      status: 500,
+    });
+  }
+
+  return redirect("/dashboard");
+};
