989b7820172be1909c1600eae5c522fa7658918e
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-05-23 15:18:05 +0700

    feat: add image bg

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,31 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-	  extend: {
-		colors: {
-		  blue01: "#0D1B2F",
-		  blue02: "#002747",
-		  yellow01: "#F3CC72",
-		  yellow02: "#FFE6AB",
-		},
-		textShadow: {
-		  glow: "0 0 2px #F3CC72, 0 0 4px #F3CC72, 0 0 6px #F3CC72, 0 0 8px #F3CC72",
-		},
-		fontFamily: {
-		  cmu: ["CMU", "serif"],
-		},
-	  },
-	},
-	plugins: [
-	  function ({ addUtilities }) {
-		const newUtilities = {
-		  ".text-shadow-glow": {
-			textShadow: "0 0 2px #F3CC72, 0 0 4px #F3CC72, 0 0 6px #F3CC72, 0 0 8px #F3CC72",
-		  },
-		};
-		addUtilities(newUtilities, ["responsive", "hover"]);
-	  },
-	],
-  };
-  
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        blue01: "#0D1B2F",
+        blue02: "#002747",
+        yellow01: "#F3CC72",
+        yellow02: "#FFE6AB",
+      },
+      textShadow: {
+        glow: "0 0 2px #F3CC72, 0 0 2px #F3CC72, 0 0 2px #F3CC72, 0 0 2px #F3CC72",
+      },
+      fontFamily: {
+        cmu: ["CMU", "serif"],
+      },
+      backgroundImage: {
+        "main-bg": "url('/images/bg.PNG')",
+      },
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".text-shadow-glow": {
+          textShadow:
+            "0 0 2px #F3CC72, 0 0 4px #F3CC72, 0 0 4px #F3CC72, 0 0 2px #F3CC72",
+        },
+      };
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
+};
