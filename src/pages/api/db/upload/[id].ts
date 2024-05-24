f1f28f9aeceda93dfeca8c38077014452eb01c65
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-05-24 10:53:02 +0700

    final v1.1 🚀

--- src/pages/api/db/upload/[id].ts
+++ src/pages/api/db/upload/[id].ts
@@ -1,66 +1,34 @@
 import type { APIRoute } from "astro";
 import { app } from "../../../../firebase/server";
 import { getFirestore } from "firebase-admin/firestore";
-import { Buffer } from "buffer"; // Import buffer
 import { getAuth } from "firebase-admin/auth";
 
 const db = getFirestore(app);
 const foreveryouthRef = db.collection("foreveryouth");
 const auth = getAuth(app);
 
-interface UploadResponse {
-  key: string;
-}
-
 export const POST: APIRoute = async ({ params, redirect, request }) => {
   const formData = await request.formData();
   const description = formData.get("description")?.toString();
-  const imageFile = formData.get("image") as File | null;
+  const imageUrl = formData.get("image_url")?.toString();
 
-  if (!imageFile) {
-    return new Response("Missing image file", {
-      status: 400,
-    });
-  }
-
-  // Read file and convert to base64
-  const arrayBuffer = await imageFile.arrayBuffer();
-  const base64Image = Buffer.from(arrayBuffer).toString("base64");
-  const user = await auth.getUser(params.id ?? "");
-  const response = await fetch(
-    "https://smo-api.bunyawatapp37204.workers.dev/images/upload",
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        body: base64Image,
-        width: 300,
-        height: 300,
-      }),
-    }
-  );
-  const jsonData: unknown = await response.json();
-  const _data = jsonData as UploadResponse;
-  const img =
-    "https://smo-api.bunyawatapp37204.workers.dev/images/" + _data.key;
-  if (!description) {
+  if (!description || !imageUrl) {
     return new Response("Missing required fields", {
       status: 400,
     });
   }
 
   if (!params.id) {
-    return new Response("Cannot find friend", {
+    return new Response("Cannot find user", {
       status: 404,
     });
   }
 
   try {
+    const user = await auth.getUser(params.id ?? "");
     await foreveryouthRef.doc(params.id).create({
       description: description,
-      image_url: img,
+      image_url: imageUrl,
       email: user.email,
     });
   } catch (error) {
@@ -74,7 +42,7 @@ export const POST: APIRoute = async ({ params, redirect, request }) => {
 
 export const DELETE: APIRoute = async ({ params, redirect }) => {
   if (!params.id) {
-    return new Response("Cannot find friend", {
+    return new Response("Cannot find user", {
       status: 404,
     });
   }
